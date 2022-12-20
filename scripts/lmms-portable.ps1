5bfe902c0ff3e0562c4f3665b902f93951e68fd1
Asmial <asiermiguel@gmail.com>
2022-12-20 09:23:20 +0100

    Fixed ci whitespace

--- scripts/lmms-portable.ps1
+++ scripts/lmms-portable.ps1
@@ -3,11 +3,10 @@ if (Test-Path "$env:USERPROFILE\.lmmsrc.xml") {
   $lmmsrc = $lmmsrc -replace "gigdir=`"[^`"]*`"", "gigdir=`"$dir\persist\workdir\samples\gig\`""
   $lmmsrc = $lmmsrc -replace "sf2dir=`"[^`"]*`"", "sf2dir=`"$dir\persist\workdir\samples\soundfonts\`""
   $lmmsrc = $lmmsrc -replace "vstdir=`"[^`"]*`"", "vstdir=`"$dir\persist\VstPlugins\`""
-  $lmmsrc = $lmmsrc -replace "laddir=`"[^`"]*`"", "laddir=`"$dir\persist\workdir\plugins\ladspa\`"" 
+  $lmmsrc = $lmmsrc -replace "laddir=`"[^`"]*`"", "laddir=`"$dir\persist\workdir\plugins\ladspa\`""
   $lmmsrc = $lmmsrc -replace "workingdir=`"[^`"]*`"", "workingdir=`"$dir\persist\workdir\`""
   $lmmsrc > "$env:USERPROFILE\.lmmsrc.xml"
-}
-else {
+} else {
   "<?xml version=`"1.0`"?>
 <!DOCTYPE lmms-config-file>
 <lmms version=`"$version`">
